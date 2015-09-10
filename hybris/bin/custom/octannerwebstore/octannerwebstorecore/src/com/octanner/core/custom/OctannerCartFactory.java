0341ddf8a352fd63e949123e00ca486051d3fac9
ferrarif789@gmail.com <ferrarif789@gmail.com>
2015-09-09 18:10:52 -0600

    Rejected commit: Default

--- hybris/bin/custom/octannerwebstore/octannerwebstorecore/src/com/octanner/core/custom/OctannerCartFactory.java
+++ hybris/bin/custom/octannerwebstore/octannerwebstorecore/src/com/octanner/core/custom/OctannerCartFactory.java
@@ -1,7 +1,12 @@
 package com.octanner.core.custom;
 
 
+import de.hybris.platform.core.model.order.CartModel;
 import de.hybris.platform.order.impl.DefaultCartFactory;
+import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.site.BaseSiteService;
+import de.hybris.platform.store.services.BaseStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Bharath on 8/18/2015.
@@ -9,5 +14,24 @@ import de.hybris.platform.order.impl.DefaultCartFactory;
 
 public class OctannerCartFactory extends DefaultCartFactory
 {
+
+    @Autowired
+    ModelService modelService;
+
+    @Autowired
+    BaseSiteService baseSiteService;
+
+    @Autowired
+    BaseStoreService baseStoreService;
+
+    @Override
+    protected CartModel createCartInternal()
+    {
+        CartModel cartInternal = super.createCartInternal();
+        cartInternal.setPurchaseOrderNumber(cartInternal.getCode());
+        cartInternal.setSite(baseSiteService.getCurrentBaseSite());
+        cartInternal.setStore(baseStoreService.getCurrentBaseStore());
+        return cartInternal;
+    }
 
 }
